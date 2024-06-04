d4a58acd4d840936c691322656de15428c2d6d2a
Luk Triquet <luktriquet@gmail.com>
2024-06-04 15:37:12 +0200

    messed a bit with bootstrap in rails, not at all completed but moving on so I can see more content today

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,10 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    question = params[:question]
-    if question =~ /i am going to work/i
+    @question = params[:question]
+    if @question =~ /i am going to work/i
       @answer = 'Great!'
-    elsif question.ends_with?('?')
+    elsif @question.ends_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
